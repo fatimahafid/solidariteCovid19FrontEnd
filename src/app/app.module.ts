46cc50941b645f4e52b053c1ae9cd7868faf5246
fatimahafid <fatima.hafid00@gmail.com>
2020-05-31 00:39:49 +0000

    commit 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AccueilIsolementComponent } from './pages/accueil-isolement/accueil-isolement.component';
+import { AccueilEnseignementComponent } from './pages/accueil-enseignement/accueil-enseignement.component';
+import { LoginComponent } from './pages/login/login.component';
+import { InscriptionComponent } from './pages/inscription/inscription.component';
+import { AccueilVoisinageComponent } from './pages/accueil-voisinage/accueil-voisinage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PagesComponent,
+    HomePageComponent,
+    AccueilIsolementComponent,
+    AccueilEnseignementComponent,
+    LoginComponent,
+    InscriptionComponent,
+    AccueilVoisinageComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [PagesComponent]
+})
+export class AppModule { }
