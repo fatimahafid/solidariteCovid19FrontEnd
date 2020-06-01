e62bf3600695e05444bc7bd73cd9d68086f67160
Elkanaany <elkanaany.21@gmail.com>
2020-06-01 12:49:50 +0100

    hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { PagesComponent } from './pages/pages.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
@@ -9,9 +8,39 @@ import { AccueilEnseignementComponent } from './pages/accueil-enseignement/accue
 import { LoginComponent } from './pages/login/login.component';
 import { InscriptionComponent } from './pages/inscription/inscription.component';
 import { AccueilVoisinageComponent } from './pages/accueil-voisinage/accueil-voisinage.component';
-import { HeaderComponent } from './pages/header/header.component';
-import { FooterComponent } from './pages/footer/footer.component';
-
+import {RouterModule, Routes} from '@angular/router';
+import {HeaderComponent} from "./pages/header/header.component";
+import {FooterComponent} from "./pages/footer/footer.component";
+const routes: Routes = [
+  {
+    path: 'inscription',
+    component: InscriptionComponent
+  },
+  {
+    path: 'voisinage',
+    component: AccueilVoisinageComponent
+  },
+  {
+    path: 'isoloment',
+    component: AccueilIsolementComponent
+  },
+  {
+    path: 'enseignement',
+    component: AccueilEnseignementComponent
+  },
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    component: HomePageComponent
+  },
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,7 +55,9 @@ import { FooterComponent } from './pages/footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
+
   ],
   providers: [],
   bootstrap: [PagesComponent]
